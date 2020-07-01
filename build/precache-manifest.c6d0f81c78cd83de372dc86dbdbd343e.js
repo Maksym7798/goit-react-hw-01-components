ede0d1e809e2e13c9fd618eb3330ab2923cfc0cb
Maksym Varfolomieiev <moav7798@gmail.com>
2020-07-01 21:11:58 +0300

    no message

---
+++ build/precache-manifest.c6d0f81c78cd83de372dc86dbdbd343e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63771d9b77425d3ce5f9c7401ea9c844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b200f40d189a6e42b09e",
+    "url": "/static/css/main.5efc5d4c.chunk.css"
+  },
+  {
+    "revision": "276e96ec34e30eca147d",
+    "url": "/static/js/2.ae3aa111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae3aa111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b200f40d189a6e42b09e",
+    "url": "/static/js/main.9a31078b.chunk.js"
+  },
+  {
+    "revision": "3fd7c41e85fe20a69193",
+    "url": "/static/js/runtime-main.1a265c15.js"
+  }
+]);
