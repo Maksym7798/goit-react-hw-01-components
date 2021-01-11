9927f48625db9a4e29074f7b48463b6e6e656bd4
Maksym Varfolomieiev <moav7798@gmail.com>
2021-01-11 21:09:12 +0200

    no message

--- build/precache-manifest.e838cf62042b5bf13f9f73d8e65ff8f9.js
+++ build/precache-manifest.e838cf62042b5bf13f9f73d8e65ff8f9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "313605c595f5fd65fbf7fec21452934c",
+    "revision": "aa15774dd95c373c50de2fbc377c7351",
     "url": "/index.html"
   },
   {
-    "revision": "e6e179698aaa5f0b20ae",
-    "url": "/static/css/main.d7ecc7fd.chunk.css"
+    "revision": "637dffcc11e042191452",
+    "url": "/static/css/main.7753cfce.chunk.css"
   },
   {
     "revision": "2d7c8c094cd3b280d669",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c819031c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e6e179698aaa5f0b20ae",
-    "url": "/static/js/main.03179f72.chunk.js"
+    "revision": "637dffcc11e042191452",
+    "url": "/static/js/main.5cd78ff1.chunk.js"
   },
   {
     "revision": "3fd7c41e85fe20a69193",
